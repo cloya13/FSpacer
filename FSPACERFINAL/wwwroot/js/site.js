701a38af64789bbd5754a7296de06492b068329d
cl964815 <cl964815@CC205-01>
2017-12-10 22:09:50 -0600

    final foreal this time

--- FSPACERFINAL/wwwroot/js/site.js
+++ FSPACERFINAL/wwwroot/js/site.js
@@ -1,31 +1,50 @@
-﻿$(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-        isClosed = false;
-
-    trigger.click(function () {
-        hamburger_cross();
+﻿//nav bar
+$(function () {
+    /* START OF DEMO JS - NOT NEEDED */
+    if (window.location == window.parent.location) {
+        $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
+        $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+        $('#fullscreen').attr('title', 'Back To Bootsnipp');
+    }
+    $('#fullscreen').on('click', function (event) {
+        event.preventDefault();
+        window.parent.location = $('#fullscreen').attr('href');
     });
+    $('#fullscreen').tooltip();
+    /* END DEMO OF JS */
 
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
+    $('.navbar-toggler').on('click', function (event) {
+        event.preventDefault();
+        $(this).closest('.navbar-minimal').toggleClass('open');
+    })
+});
+function htmlbodyHeightUpdate() {
+    var height3 = $(window).height()
+    var height1 = $('.nav').height() + 50
+    height2 = $('.main').height()
+    if (height2 > height3) {
+        $('html').height(Math.max(height1, height3, height2) + 10);
+        $('body').height(Math.max(height1, height3, height2) + 10);
+    }
+    else {
+        $('html').height(Math.max(height1, height3, height2));
+        $('body').height(Math.max(height1, height3, height2));
     }
 
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
+}
+$(document).ready(function () {
+    htmlbodyHeightUpdate()
+    $(window).resize(function () {
+        htmlbodyHeightUpdate()
     });
-});// Write your JavaScript code.
+    $(window).scroll(function () {
+        height2 = $('.main').height()
+        htmlbodyHeightUpdate()
+    });
+});
+
+
+// Write your JavaScript code.
 
 
 //search index
